c753b6b8c10f7fc3080d132cd23a58bd72d2064a
paradoxial-composition <elbani.othman@gmail.com>
2021-09-29 09:34:53 +0100

    Add info to the hero component

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,19 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hi, I m Oto <br />
+        Welcome to my virtual business card.
+      </SectionTitle>
+      <SectionText>
+        I am a JavaScritp web app developper with a sense of adventure and want for new experiences, I am light minded, fast learner and adaptive.
+      </SectionText>
+      <Button onClick={() => window.location = 'https://google.com'}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
